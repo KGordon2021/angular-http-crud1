65e636071c9326a74faad6ea4655be9de3c77c43
KGordon <krisan_gordon@yahoo.com>
2022-07-06 12:44:20 -0500

    added selected Items component. It is working now!!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SelectedItemComponent } from './selected-item/selected-item.component';
+import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'selectedItems/:id', component: SelectedItemComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'orders', component: OrdersComponent}
 ];
 
 @NgModule({
